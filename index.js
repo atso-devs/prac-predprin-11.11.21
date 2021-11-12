7618514cfb9344589e40a38f3b40fc6bbabe3215
nickxgrom <52834219+nickxgrom@users.noreply.github.com>
2021-11-12 17:43:47 +0600

    add(index): task3

--- index.js
+++ index.js
@@ -24,3 +24,15 @@ const pObj = {
 }
 console.log('Задание 2:');
 console.log(`П'пред = ${pObj.prev}\nП'отч = ${pObj.report}`);
+
+// TODO: нужно уточнить настчет процентов
+// task3
+// rProcents - это то, на сколько процентов нужно умножать, 
+// в задании может быть число отличное от 100
+const rProcents = 100
+const rObj = {
+    prev:  ( (pObj.prev/data.z.prev) * rProcents/100 ).toFixed(2),
+    report: ( (pObj.report/data.z.report) * rProcents/100 ).toFixed(2),
+}
+console.log('Задание 3:');
+console.log(`R'пред = ${rObj.prev}\nR'отч = ${rObj.report}`);
